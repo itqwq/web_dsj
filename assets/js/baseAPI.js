5e0bb85d5a97a027a6a35839db57bbb16f5c74ce
itqwq <1593690852@qq.com>
2023-03-20 12:16:38 +0800

    完成了主页的编写

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,5 +5,23 @@ $.ajaxPrefilter(function(options) {
    console.log(options.url);
    //在真正发起的 ajax 请求之前，统一拼接请求的根路径
    options.url = 'http://www.liulongbin.top:3007' + options.url
-   console.log(options.url);
+   // console.log(options.url);
+   // 统一设置有权限的接口 设置请求头 indexOf()是判断的方法
+   if(options.url.indexOf('/my/') !== -1 ){
+      options.headers = {
+         Authorization: localStorage.getItem('token')
+       }
+   }
+
+   //全局挂在complete
+   options.complete = function(res){
+      // console.log(res);
+      //在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应的数据
+      if(res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！'){
+         //1.清空存储数据
+         localStorage.removeItem('token')
+         //强制跳转登录页面
+         location.href='./login.html'
+       }
+    }
 })
